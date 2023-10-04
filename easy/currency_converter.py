a7424f2a34e328fe8fdf79f2388286630d538c0f
parksuha <parksuha@msu.edu>
2023-10-04 11:58:26 -0400

    update

--- easy/currency_converter.py
+++ easy/currency_converter.py
@@ -1,12 +1,6 @@
 def convert(dollars):
-    if dollars < 0:
-        return 'Cannot have negative value'
-    
-    header = "This program converts US dollars to Korean Won\n"
+    pass
+    # your code goes here
 
-    won = convert_to_won(dollars)
-    
-    result = header + f'${dollars} is {won} won'
-    return result
-
-convert_to_won = lambda dollars: dollars * 1356.62
+# lambda function
+# your code goes here
